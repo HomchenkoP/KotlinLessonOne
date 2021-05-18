28d17a2e8c06e9648e1eaf98f6125383935bf6b3
HomchenkoP <homchenkop@yandex.ru>
2021-05-18 19:57:13 +0300

    b. Создать Object. В Object вызвать copy и вывести значения скопированного класса на экран.

--- app/src/main/java/ru/geekbrains/kotlinlessonone/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/kotlinlessonone/MainActivity.kt
@@ -8,6 +8,9 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    var evenOdd: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,6 +32,28 @@ class MainActivity : AppCompatActivity() {
 
         propertyOneTextView.setText(someDataClass.propOne)
         propertyTwoTextView.setText(someDataClass.propTwo.toString())
+
+        var dataClassCopy: SomeDataClass
+
+        val propertyOneCopy: TextView = findViewById(R.id.propertyOneCopy)
+        val propertyTwoCopy: TextView = findViewById(R.id.propertyTwoCopy)
+
+        val buttonCopy: Button = findViewById(R.id.buttonCopy)
+        buttonCopy.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                if (evenOdd == 0) {
+                    evenOdd = 1
+                    // propOne - новое значение, propTwo - скопированное
+                    dataClassCopy = someDataClass.copy(propOne = "Copy")
+                } else {
+                    evenOdd = 0
+                    // propOne - скопированное значение, propTwo - новое
+                    dataClassCopy = someDataClass.copy(propTwo = 11111)
+                }
+                propertyOneCopy.setText(dataClassCopy.propOne)
+                propertyTwoCopy.setText(dataClassCopy.propTwo.toString())
+            }
+        })
     }
 
     val buttonOnClickListener: View.OnClickListener = object : View.OnClickListener {
