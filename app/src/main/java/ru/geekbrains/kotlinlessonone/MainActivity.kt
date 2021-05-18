c51506565f6e46dbcdc3287e2d5460851a317a1f
HomchenkoP <homchenkop@yandex.ru>
2021-05-18 19:43:33 +0300

    5. Потренироваться в создании классов и функций, описанных в уроке, и убедиться, что всё работает. Например, создать тестовое приложение: a. Сформировать data class с двумя свойствами и вывести их на экран приложения.

--- app/src/main/java/ru/geekbrains/kotlinlessonone/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/kotlinlessonone/MainActivity.kt
@@ -3,6 +3,7 @@ package ru.geekbrains.kotlinlessonone
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,15 +12,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var button: Button = findViewById(R.id.button)
+        val button: Button = findViewById(R.id.button)
         button.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
                 Toast.makeText(applicationContext, "Кнопка нажата.", Toast.LENGTH_SHORT).show()
             }
         })
 
-        var buttonTwo: Button = findViewById(R.id.buttonTwo)
+        val buttonTwo: Button = findViewById(R.id.buttonTwo)
         buttonTwo.setOnClickListener(buttonOnClickListener)
+
+        val someDataClass: SomeDataClass = SomeDataClass("Свойство", 2)
+
+        val propertyOneTextView: TextView = findViewById(R.id.propertyOne)
+        val propertyTwoTextView: TextView = findViewById(R.id.propertyTwo)
+
+        propertyOneTextView.setText(someDataClass.propOne)
+        propertyTwoTextView.setText(someDataClass.propTwo.toString())
     }
 
     val buttonOnClickListener: View.OnClickListener = object : View.OnClickListener {
---
+++ app/src/main/java/ru/geekbrains/kotlinlessonone/SomeDataClass.kt
@@ -0,0 +1,3 @@
+package ru.geekbrains.kotlinlessonone
+
+data class SomeDataClass(var propOne: String, var propTwo: Int)
